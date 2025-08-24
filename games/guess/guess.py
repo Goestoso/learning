869ec88d70a61a3b39a8f6c72b77c84443f204c3
Goes <gabriel.goes2107@gmail.com>
2025-08-24 13:10:12 -0300

    Relase do jogo Aritificial Guess - v1.0.0

--- games/guess/guess.py
+++
@@ -1,6 +0,0 @@
-"""Guess Entry Point"""
-
-from src import main
-
-if(__name__ == "__main__"): # contexto de execução do programa
-    main()
--- games/guess/src/__init__.py
+++
@@ -1,9 +0,0 @@
-# src/__init__.py
-
-from . import utils
-from . import gui
-from . import logger
-from .main import main
-
-__version__ = "1.0.0"
-__author__ = "Artificial"
